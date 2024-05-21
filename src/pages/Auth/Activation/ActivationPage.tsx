ea6e2bfc9e83cb5cfa23fa5093b9f250397ee87c
ZXCBaron <alwx202@mail.ru>
2024-05-21 15:12:33 +0300

    Added blyat already third time the links from activation page to 'profile' and 'home'. Also, a bit upgraded design of the block.

--- src/pages/Auth/Activation/ActivationPage.tsx
+++ src/pages/Auth/Activation/ActivationPage.tsx
@@ -4,6 +4,8 @@ import {Navigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import {RootState} from "src/app/providers/storeProvider";
 import Block from "src/shared/ui/Block/Block";
+import MyLink from "src/shared/ui/Link/Link";
+import cls from './ActivationPage.module.scss'
 
 const ActivationPage: FC = () => {
 	const user = useSelector((state: RootState) => state.user);
@@ -11,13 +13,17 @@ const ActivationPage: FC = () => {
 	if (user.user?.isActivated === 1) {
 		toast.info("Вы подтвердили почту");
 		return <Navigate to={"/"} />;
-	}
+}
 	return (
-		<Block>
+		<Block className={cls.MainBlock}>
 			<h1>Hello</h1>
-			<span>
+			<span className={cls.EmailNotification}>
 				На почту {user.user?.email} отправлена ссылка для подтветждения
 			</span>
+			<ul className={cls.LinksList}>
+				<MyLink to="/">Home</MyLink>
+				<MyLink to="/profile">Profile</MyLink>
+			</ul>
 		</Block>
 	);
 };
