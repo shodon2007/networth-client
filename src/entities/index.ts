1d6032b9c52d60dfbb7cba2ac04950388ba6b0d5
asrorovirbis <asrorovirbis@gmail.com>
2024-06-04 15:20:54 +0700

    update code

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,3 @@
-import {UserType} from "./user";
-import {userApi} from "./user/services/userService";
 import userSlice, {setUser} from "./user/services/userSlice";
 
-export type {UserType};
-export {userApi, userSlice, setUser};
+export {userSlice, setUser};
