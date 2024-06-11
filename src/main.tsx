19806b7a2602c52e68fcec42cd7c231ab500eb5b
ZXCBaron <alwx202@mail.ru>
2024-06-11 10:35:18 +0300

    Some changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
 import {BrowserRouter} from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
 
 import App from "./app/App";
 import "./app/styles/index.scss";
 import {StoreProvider} from "./app/providers/storeProvider";
 
 import "src/shared/config/i18n/i18n";
+import { QueryProvider } from "./app/providers";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<QueryClientProvider client={new QueryClient()}>
+	<QueryProvider>
 		<StoreProvider>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
 		</StoreProvider>
-	</QueryClientProvider>,
+	</QueryProvider>
 );
