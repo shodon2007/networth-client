4eb37d32b149bf188a5977a93e6b2bf8085f3759
ZXCBaron <alwx202@mail.ru>
2024-07-14 18:07:10 +0300

    aaa

--- src/entities/messenger/ui/WSBtn.tsx
+++ src/entities/messenger/ui/WSBtn.tsx
@@ -1,22 +1,24 @@
 import useWebSocket from "react-use-websocket";
 
-interface WSBtnProps {}``
+interface WSBtnProps {}
 
 export const WSBtn = ({}: WSBtnProps) => {
 
   // ! Connect to the *WS* server
-  const WS = useWebSocket("ws://localhost:8080", {
-    onMessage: message => console.log(message.data),
-    onError: (err) => console.error(err),
-    onOpen: data => console.log(data),
-    share: true, // Allow to use anywhere in our app
-  });
 
-  // * Send a message to the *WS* server
-  const SendWSMsg = () => {
-    WS.sendMessage('Holla!');
-  };
+    const WS = useWebSocket("wss://networth.shodon.ru/ws", {
+      onMessage: message => console.log('Message ->', message),
+      onOpen: data => console.log('Connected', data),
+      onError: (err) => console.error("Connection is broke ->", err),
+      share: true, // Allow to use anywhere in our app
+    });
   
+    // * Send a message to the *WS* server
+    const SendWSMsg = () => {
+      WS.sendJsonMessage({msg: "Holla!", id: 2312});
+    }; 
+  
+
 
 	return (
 		<button onClick={() => SendWSMsg()}>
@@ -24,42 +26,3 @@ export const WSBtn = ({}: WSBtnProps) => {
 		</button>
 	);
 };
-
-
-/* 
-* Below the work server for the code above :). 
-const http = require('http')
-const express = require('express')
-const WebSocket = require('ws')
-const { monitorEventLoopDelay } = require('perf_hooks')
-const { log } = require('console')
-
-const app = express()
-
-const server = http.createServer(app)
-
-const wsServer = new WebSocket.Server({ server })
-
-wsServer.on('connection', ws => {
-  console.log(ws)
-  ws.on('message', m => {
-    console.log(m.toString())
-    wsServer.clients.forEach(client => client.send(m))
-  })
-
-
-  ws.on('error', err => ws.send(err))
-
-  ws.send("Hi there! You're connected to a WebSocket server")
-})
-
-wsServer.on('close', user => {
-  console.log(`User has been disconnected`)
-  
-})
-
-server.listen(8080, () => {
-  console.log('server listening on port', 8080)
-  
-})
-*/
