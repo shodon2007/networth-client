a1655f5b11286ea1a2fa9a33829faf3c6541f7a6
ZXCBaron <alwx202@mail.ru>
2024-07-14 18:37:38 +0300

    firxe in merges

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -4,15 +4,14 @@ import {SearchBar} from "src/features/SearchBar/";
 import cls from "./Header.module.scss";
 import {ThemeSwitcher} from "src/features/ThemeSwitcher";
 import {ToggleLanguage} from "src/features/ToggleLanguage";
-import { useTranslation } from "react-i18next";
-import { useTheme } from "src/shared/lib/theme";
 
 export const Header = () => {
-	const { theme, toggleTheme } = useTheme();
-	const { i18n } = useTranslation();
-
-	const toggleLanguage = () => {
-		void i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
-	};
-
+	return (
+		<div className={cls.header}>
+			<img src={NetWorth} className={cls.logo} />
+			<SearchBar />
+			<ThemeSwitcher />
+			<ToggleLanguage />
+		</div>
+	);
 };
