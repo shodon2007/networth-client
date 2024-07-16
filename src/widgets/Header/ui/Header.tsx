156c36bcb362d9eae14c37ecfe8ea60b574f6514
shodon2007 <asrorovirbis@gmail.com>
2024-07-16 08:24:50 +0700

    change sidebar and header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -4,14 +4,32 @@ import {SearchBar} from "src/features/SearchBar/";
 import cls from "./Header.module.scss";
 import {ThemeSwitcher} from "src/features/ThemeSwitcher";
 import {ToggleLanguage} from "src/features/ToggleLanguage";
+import {useUser} from "src/entities/user";
+import MyLink from "src/shared/ui/Link/Link";
+import {routePath} from "src/shared/config/routeConfig";
 
 export const Header = () => {
+	const user = useUser();
+
+	const {data: userData} = user ?? {};
+
 	return (
 		<div className={cls.header}>
-			<img src={NetWorth} className={cls.logo} />
-			<SearchBar />
-			<ThemeSwitcher />
-			<ToggleLanguage />
+			<div className={cls.left}>
+				<img src={NetWorth} className={cls.logo} />
+				<SearchBar />
+			</div>
+			{/* <ThemeSwitcher />
+			<ToggleLanguage /> */}
+			<div className={cls.rightAvatar}>
+				<img src={userData?.avatar} alt="avatar" className={cls.avatar} />
+				{!userData && (
+					<div className={cls.headerLinks}>
+						<MyLink to={routePath.login}>Войти</MyLink>
+						<MyLink to={routePath.registration}>Зарегестрироваться</MyLink>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,9 +1,7 @@
 import {FC} from "react";
 import {useTranslation} from "react-i18next";
 import {NavLink} from "react-router-dom";
-import {useUser} from "src/entities/user";
 import classNames from "src/shared/lib/classNames";
-import MyLink from "src/shared/ui/Link/Link";
 
 import links from "../model/Links";
 
@@ -17,26 +15,8 @@ interface SidebarProps {}
 
 const Sidebar: FC<SidebarProps> = () => {
 	const {t} = useTranslation();
-	const user = useUser();
-
-	const {data: userData} = user ?? {};
-
 	return (
 		<aside className={cls.sidebar}>
-			<header className={cls.header}>
-				<img src={userData?.avatar} alt="avatar" className={cls.avatar} />
-				{userData ? (
-					<h1 className={cls.username}>
-						<span>{userData.name}</span>
-						<span>{userData.surname}</span>
-					</h1>
-				) : (
-					<div className={cls.headerLinks}>
-						<MyLink to={"/login"}>Войти</MyLink>
-						<MyLink to={"/login"}>Зарегестрироваться</MyLink>
-					</div>
-				)}
-			</header>
 			<nav className={cls.navbar}>
 				{links.map((link) => {
 					return (
