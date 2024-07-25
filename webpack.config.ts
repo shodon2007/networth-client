13f5079c19134c3e6fb84ddc8dd8e86785ea412d
shodon2007 <asrorovirbis@gmail.com>
2024-07-25 11:54:34 +0700

    fix searchUser and update code

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,11 +4,16 @@ import buildConfig from "./config/build/buildConfig";
 import {BuildEnv, BuildOptions, BuildPaths} from "./config/build/types/config";
 
 export default (env: BuildEnv): Configuration => {
+	const mode: BuildOptions["mode"] = env.mode ?? "development";
+	const isDev = mode === "development";
+
 	const paths: BuildPaths = {
 		entry: path.resolve(__dirname, "src", "index.tsx"),
 		html: path.resolve(__dirname, "public", "index.html"),
 		output: path.resolve(__dirname, "dist"),
-		env: path.resolve(__dirname, ".env"),
+		env: isDev
+			? path.resolve(__dirname, "dev.env")
+			: path.resolve(__dirname, "prod.env"),
 	};
 	const PORT = env.port ?? 9889;
 	const options: BuildOptions = {
